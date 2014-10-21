ef498f21fa28322b2b1546b420eebbdb3279abeb
Johannes <zusitools@web.de>
2014-10-21 20:38:47 +0200

    Remove unnecessary print statement

--- i18n.py
+++ i18n.py
@@ -5,4 +5,3 @@ import os
 script_path = os.path.dirname(inspect.getfile(inspect.currentframe()))
 l10n_path = os.path.join(script_path, 'l10n')
 language = gettext.translation('blender_ls3', l10n_path, languages=['de'], fallback=True)
-print(language)
