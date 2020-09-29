zusitools/blender_ls3

commit 25b4a5d576b1a3455e36c64cf6e056a927d5aefe

Allow writing LSB files and make it the default

__init__.py
@@ -389,6 +389,12 @@ class EXPORT_OT_ls3(bpy.types.Operator, ExportHelper):
         max = 360.0
     )
 
+    writeLsb = bpy.props.BoolProperty(
+        name = _("Write mesh data to LSB file"),
+        description = _("Write vertex and face data to a separate file for better loading performance in Zusi"),
+        default = ls3_export.get_exporter_setting("writeLsb"),
+    )
+
     variant_export_setting = bpy.props.CollectionProperty(
         name = _("Variants to export"),
         description = _("Choose which variants to export"),
@@ -446,6 +452,7 @@ class EXPORT_OT_ls3(bpy.types.Operator, ExportHelper):
         col.prop(self, "maxCoordDelta")
         col.prop(self, "maxUVDelta")
         col.prop(self, "maxNormalAngle")
+        layout.prop(self, "writeLsb")
 
     def get_exporter(self, context):
         if self.properties.exportSelected == '4':  # Visible layers
@@ -466,6 +473,7 @@ class EXPORT_OT_ls3(bpy.types.Operator, ExportHelper):
             self.properties.maxUVDelta,
             self.properties.maxCoordDelta,
             self.properties.maxNormalAngle,
+            self.properties.writeLsb,
             [setting.variant_id for setting in self.properties.variant_export_setting if setting.export == True],
             selectedObjects,
         )

ls3_export.py
@@ -50,6 +50,7 @@ default_export_settings = {
     "maxCoordDelta" : 0.001,
     "maxUVDelta" : 0.002,
     "maxNormalAngle" : radians(10),
+    "writeLsb" : True,
 }
 
 registry_export_settings = None
@@ -60,7 +61,7 @@ def get_registry_export_settings():
         import winreg
         values = zusicommon.read_registry_strings([
             (winreg.HKEY_CURRENT_USER, "Software\\Zusi3\\3DEditor\\Einstellungen"),
-        ], set(["MeshOptimierungAbstand", "MeshOptimierungUV", "MeshOptimierungWinkel"]))
+        ], set(["MeshOptimierungAbstand", "MeshOptimierungUV", "MeshOptimierungWinkel", "xmlls3Schreiben"]))
         if values is not None:
             if "MeshOptimierungAbstand" in values:
                 result["maxCoordDelta"] = struct.unpack("d", values["MeshOptimierungAbstand"])[0]
@@ -68,6 +69,8 @@ def get_registry_export_settings():
                 result["maxUVDelta"] = struct.unpack("d", values["MeshOptimierungUV"])[0]
             if "MeshOptimierungWinkel" in values:
                 result["maxNormalAngle"] = struct.unpack("d", values["MeshOptimierungWinkel"])[0]
+            if "xmlls3Schreiben" in values:
+                result["writeLsb"] = (values["xmlls3Schreiben"] == 0)
     except Exception:
         pass
 
@@ -402,6 +405,7 @@ class Ls3ExporterSettings:
                 maxUVDelta,
                 maxCoordDelta,
                 maxNormalAngle,  # in radians
+                writeLsb,
                 variantIDs = [],
                 selectedObjects = [],
                 ):
@@ -415,6 +419,7 @@ class Ls3ExporterSettings:
         self.maxUVDelta = maxUVDelta
         self.maxCoordDelta = maxCoordDelta
         self.maxNormalAngle = maxNormalAngle
+        self.writeLsb = writeLsb
         self.variantIDs = variantIDs
         self.selectedObjects = selectedObjects
 
@@ -424,11 +429,6 @@ class Ls3Exporter:
 
         # The XML document node
         self.xmldoc = None
-
-        try:
-            self.use_lsb = zusiconfig.use_lsb and not os.getenv('NO_LSB')
-        except:
-            self.use_lsb = False
 
         # Initialize map of Blender Z bias values (float) to integer values
         # e.g. if values (-0.1, -0.05, 0, 0.1) appear in the scene, they will be
@@ -622,7 +622,7 @@ class Ls3Exporter:
             if material.zusi_second_pass and material.zusi_texture_preset == '4':
                 subsetNode.setAttribute("DoppeltRendern", "1")
 
-        if not self.use_lsb:
+        if not self.config.writeLsb:
             # Generating all <Vertex> and <Face> nodes via the xmldoc functions is slooooow.
             # Therefore, a special node is inserted that generates the necessary <Vertex> and
             # <Face> XML on the fly. Yes, this is ugly.
@@ -1313,7 +1313,7 @@ class Ls3Exporter:
             ls3file.filename)
 
         lsbwriter = None
-        if self.use_lsb and sum(len(subset.vertexdata) + len(subset.facedata) for subset in ls3file.subsets) > 0:
+        if self.config.writeLsb and any(len(subset.vertexdata) > 0 or len(subset.facedata) > 0 for subset in ls3file.subsets):
             (basename, ext) = os.path.splitext(filepath)
             lsbpath = basename + ".lsb"
         

tests/ls3_export_test.py
@@ -74,11 +74,11 @@ class TestLs3Export(unittest.TestCase):
     sys.modules["io_scene_ls3.zusiconfig"].datapath = ZUSI3_DATAPATH
     sys.modules["io_scene_ls3.zusiconfig"].datapath_official = ZUSI3_DATAPATH_OFFICIAL
     sys.modules["io_scene_ls3.zusiconfig"].z2datapath = ZUSI2_DATAPATH
-    sys.modules["io_scene_ls3.zusiconfig"].use_lsb = False
     sys.modules["io_scene_ls3.zusiconfig"].default_export_settings = {
         "exportAnimations" : False,
         "optimizeMesh" : False,
         "exportSelected" : '0',
+        "writeLsb" : False,
     }
 
   def tearDown(self):
@@ -286,8 +286,7 @@ class TestLs3Export(unittest.TestCase):
     self.assertEqual(set([("Author 1", "0"), ("Author 2", "5")]), licenses)
 
   def test_lsb_node_pos(self):
-    sys.modules["io_scene_ls3.zusiconfig"].use_lsb = True
-    root = self.export_and_parse()
+    root = self.export_and_parse({ "writeLsb": True })
 
     landschaft_node = root.find("./Landschaft")
     lsb_node = root.find("./Landschaft/lsb")
@@ -297,9 +296,8 @@ class TestLs3Export(unittest.TestCase):
         landschaft_node.getchildren().index(lsb_node))
 
   def test_no_lsb_on_empty(self):
-    sys.modules["io_scene_ls3.zusiconfig"].use_lsb = True
     self.clear_scene()
-    root = self.export_and_parse()
+    root = self.export_and_parse({ "writeLsb": True })
 
     self.assertFalse(os.path.exists(os.path.join(ZUSI3_EXPORTPATH, "export.lsb")))
     self.assertEqual([], root.findall("./Landschaft/lsb"))

tests/ls3_import_test.py
@@ -32,7 +32,6 @@ class TestLs3Import(unittest.TestCase):
     sys.modules["io_scene_ls3.zusiconfig"].datapath = ZUSI3_DATAPATH
     sys.modules["io_scene_ls3.zusiconfig"].datapath_official = ZUSI3_DATAPATH_OFFICIAL
     sys.modules["io_scene_ls3.zusiconfig"].z2datapath = ZUSI2_DATAPATH
-    sys.modules["io_scene_ls3.zusiconfig"].use_lsb = False
 
     self._mock_fs = MockFS()
     self._mock_fs.start()
@@ -87,7 +86,6 @@ class TestLs3Import(unittest.TestCase):
   # Tests start here
   # ---
   def test_import_lsb(self):
-    sys.modules["io_scene_ls3.zusiconfig"].use_lsb = True
     self.ls3_import("lsb.ls3")
     self.assertEqual(12, len(bpy.data.objects["lsb.ls3.0"].data.polygons))
 

commit 2118c84d1f2331747a9ddcb9647c6ce55fb532db

Add writeLsb setting to EXPORT_OT_ls3_batch
Fixes: 25b4a5d576b1a3455e36c64cf6e056a927d5aefe

__init__.py
@@ -580,6 +580,7 @@ class EXPORT_OT_ls3_batch(bpy.types.Operator):
                     ls3_export.get_exporter_setting("maxUVDelta"),
                     ls3_export.get_exporter_setting("maxCoordDelta"),
                     ls3_export.get_exporter_setting("maxNormalAngle"),
+                    ls3_export.get_exporter_setting("writeLsb"),
                     variantIDs = setting[1],
                     selectedObjects = setting[0],
                 )