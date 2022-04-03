zusitools/blender_ls3

commit c5337cd307614c13f37b643a311c0e8c787a7385

[ls3_export] Simplify normals calculation

ls3_export.py
@@ -658,10 +658,8 @@ class Ls3Exporter:
         mesh.calc_loop_triangles()
 
         # If the object is mirrored/negatively scaled, the normals will come out the wrong way
-        # when applying the transformation. Workaround from:
-        # http://projects.blender.org/tracker/index.php?func=detail&aid=18834&group_id=9&atid=264
-        ma = ob.matrix_world.to_3x3() # gets the rotation part
-        must_flip_normals = Vector.dot(ma[2], Vector.cross(ma[0], ma[1])) >= 0.00001
+        # when applying the transformation.
+        must_flip_normals = transform_matrix.determinant() < 0.0
 
         # List vertex indices of edges that are marked as "sharp edges",
         # which means we won't merge them later during mesh optimization.
@@ -735,24 +733,24 @@ class Ls3Exporter:
                     normal = (0, 0, 1)
                 else:
                     if use_auto_smooth:
-                        normal = Vector((loop.normal[1], -loop.normal[0], -loop.normal[2]))
+                        normal = loop.normal
                     elif face.use_smooth:
-                        normal = Vector((v.normal[1], -v.normal[0], -v.normal[2]))
+                        normal = v.normal
                         for g in v.groups:
                             if g.weight == 0.0:
                                 continue
-                            if g.group == vgroup_xy:
-                                normal[2] = 0
-                            elif g.group == vgroup_yz:
-                                normal[1] = 0
-                            elif g.group == vgroup_xz:
+                            if g.group == vgroup_yz:
                                 normal[0] = 0
+                            elif g.group == vgroup_xz:
+                                normal[1] = 0
+                            elif g.group == vgroup_xy:
+                                normal[2] = 0
                         normal.normalize()
                     else:
-                        normal = (face.normal[1], -face.normal[0], -face.normal[2])
+                        normal = face.normal
 
                     if must_flip_normals:
-                        normal = (-normal[0], -normal[1], -normal[2])
+                        normal = -normal
 
                 # Calculate square of vertex length (projected onto the XY plane)
                 # for the bounding radius.
@@ -764,7 +762,7 @@ class Ls3Exporter:
                 # The vertex index is appended for reordering vertices
                 subset.vertexdata.append((
                     -v.co[1], v.co[0], v.co[2],
-                    normal[0], normal[1], normal[2],
+                    -normal[1], normal[0], normal[2],
                     uvdata1[0], 1 - uvdata1[1],
                     uvdata2[0], 1 - uvdata2[1],
                     maxvertexindex + i,

commit e2e619baf0b33d62152743ec738ec5916715d7d9

[ls3_export] Fix winding order of exported triangles
Fixes: c5337cd307614c13f37b643a311c0e8c787a7385

ls3_export.py
@@ -648,20 +648,21 @@ class Ls3Exporter:
         transform_matrix = self.transformation_relative(ob_original, self.get_animated_ob(ob_original), ls3file.root_obj)
         mesh.transform(transform_matrix)
 
-        if bpy.app.version == (3, 1, 0):
-            # Workaround for a Blender bug
-            mesh.flip_normals()
+        # transform() documentation: "Warning: inverts normals if matrix is negative"
+        must_flip_normals = transform_matrix.determinant() < 0.0
+        if must_flip_normals:
             mesh.flip_normals()
         else:
-            mesh.calc_normals()
+            if bpy.app.version == (3, 1, 0):
+                # Workaround for https://developer.blender.org/T96999
+                mesh.flip_normals()
+                mesh.flip_normals()
+            else:
+                mesh.calc_normals()
         use_auto_smooth = mesh.use_auto_smooth
         if mesh.use_auto_smooth:
             mesh.calc_normals_split()
         mesh.calc_loop_triangles()
-
-        # If the object is mirrored/negatively scaled, the normals will come out the wrong way
-        # when applying the transformation.
-        must_flip_normals = transform_matrix.determinant() < 0.0
 
         # List vertex indices of edges that are marked as "sharp edges",
         # which means we won't merge them later during mesh optimization.
@@ -699,12 +700,8 @@ class Ls3Exporter:
             subset = subsets[face.material_index]
             maxvertexindex = len(subset.vertexdata)
 
-            # Write the triangle.
-            # Optionally reverse order of faces to flip normals
-            if must_flip_normals:
-                subset.facedata.extend((maxvertexindex + 2, maxvertexindex + 1, maxvertexindex))
-            else:
-                subset.facedata.extend((maxvertexindex, maxvertexindex + 1, maxvertexindex + 2))
+            # Write the triangle, fixing the winding order.
+            subset.facedata.extend((maxvertexindex + 2, maxvertexindex + 1, maxvertexindex))
 
             # Compile a list of all vertices to mark as "don't merge".
             # Those are the vertices that form a sharp edge in the current face.
@@ -750,9 +747,6 @@ class Ls3Exporter:
                         normal.normalize()
                     else:
                         normal = face.normal
-
-                    if must_flip_normals:
-                        normal = -normal
 
                 # Calculate square of vertex length (projected onto the XY plane)
                 # for the bounding radius.

tests/ls3_export_test.py
@@ -367,6 +367,35 @@ class TestLs3Export(unittest.TestCase):
       self.assertAlmostEqual(0, float(vertex_node.attrib["U2"]), 5)
       self.assertAlmostEqual(0, float(vertex_node.attrib["V2"]), 5)
 
+  def test_negative_scale(self):
+    self.open("negative_scale")
+    root = self.export_and_parse()
+    p_nodes = root.findall("./Landschaft/SubSet/Vertex/p")
+    n_nodes = root.findall("./Landschaft/SubSet/Vertex/n")
+    self.assertEqual(12, len(p_nodes))
+    self.assertEqual(12, len(n_nodes))
+
+    # Check winding order by calculating the surface normal
+    face_nodes = root.findall("./Landschaft/SubSet/Face")
+    self.assertEqual(4, len(face_nodes))
+    for face_node in face_nodes:
+      vertex_indices = [int(i) for i in face_node.attrib["i"].split(';')]
+      self.assertEqual(3, len(vertex_indices))
+
+      coords = [
+        mathutils.Vector([float(p_nodes[vertex_indices[i]].attrib[name]) for name in "XYZ"])
+        for i in range(3)
+      ]
+      normal = (coords[2] - coords[0]).cross(coords[1] - coords[0]).normalized()
+      self.assertAlmostEqual(0, normal[0], places=5)
+      self.assertAlmostEqual(0, normal[1], places=5)
+      self.assertAlmostEqual(1, normal[2], places=5)
+
+    for v in n_nodes:
+      self.assertAlmostEqual(0, float(v.attrib["X"]), places=5)
+      self.assertAlmostEqual(0, float(v.attrib["Y"]), places=5)
+      self.assertAlmostEqual(1, float(v.attrib["Z"]), places=5)
+
   def test_export_edit_mode(self):
     self.open("edit_mode")
     bpy.ops.object.editmode_toggle()