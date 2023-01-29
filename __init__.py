zusitools/blender_ls3

commit 2df7840d5de5c8e9a2b18788cc503dadbe8df272

[init] Register list class with Blender

__init__.py
@@ -141,7 +141,7 @@ class ZusiLodImportSetting(bpy.types.PropertyGroup):
 
 bpy.utils.register_class(ZusiLodImportSetting)
 
-class ZusiLodImportSettingList(zusiprops.CheckBoxList, bpy.types.UIList):
+class ZUSI_UL_ZusiLodImportSettingList(zusiprops.CheckBoxList, bpy.types.UIList):
     def get_property_name(self):
         return "imp"
     
@@ -679,10 +679,10 @@ def menu_func_show_variants(self, context):
 
 classes = (
 #    ZusiFileVariantExportSetting,
-#    ZusiFileVariantExportSettingList,
+    ZUSI_UL_ZusiFileVariantExportSettingList,
 #    ZusiFileVariantVisibilitySetting,
 #    ZusiLodImportSetting,
-#    ZusiLodImportSettingList,
+    ZUSI_UL_ZusiLodImportSettingList,
     IMPORT_OT_ls,
     IMPORT_OT_ls3,
     OBJECT_OT_embed_linked,

commit ddb7bc92ebaff52132ba8b2fd838c7b7c2fdf883

[init] Fix class name in template_list call
Fixes: 2df7840d5de5c8e9a2b18788cc503dadbe8df272

__init__.py
@@ -245,7 +245,7 @@ class IMPORT_OT_ls3(bpy.types.Operator, ImportHelper):
         layout.label(text=_("Import LODs (only embedded linked files)"))
         row = layout.row()
         row.enabled = self.properties.loadLinkedMode == "2"
-        row.template_list("ZusiLodImportSettingList", "", self, "lod_import_setting", self, "lod_import_setting_index")
+        row.template_list("ZUSI_UL_ZusiLodImportSettingList", "", self, "lod_import_setting", self, "lod_import_setting_index")
 
     def execute(self, context):
         from . import ls3_import