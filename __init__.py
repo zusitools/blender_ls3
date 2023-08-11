zusitools/blender_ls3

commit 233337fbe287bbc3d41f310304078433faa75184

Rename ZusiFileVariantExportSettingList -> ZUSI_UL_ZusiFileVariantExportSettingList

__init__.py
@@ -95,7 +95,7 @@ class ZusiFileVariantExportSetting(bpy.types.PropertyGroup):
 
 bpy.utils.register_class(ZusiFileVariantExportSetting)
 
-class ZusiFileVariantExportSettingList(zusiprops.CheckBoxList, bpy.types.UIList):
+class ZUSI_UL_ZusiFileVariantExportSettingList(zusiprops.CheckBoxList, bpy.types.UIList):
     def get_property_name(self):
         return "export"
     

commit c3656956c8fd58cb83144980df0e9eff9c5f79d7

[init] Fix name of layout.template_list argument
Fixes: 233337fbe287bbc3d41f310304078433faa75184

__init__.py
@@ -429,7 +429,7 @@ class EXPORT_OT_ls3(bpy.types.Operator, ExportHelper):
 
         if len(context.scene.zusi_variants) > 0:
             num_rows = min(5, len(self.properties.variant_export_setting))
-            layout.template_list("ZusiFileVariantExportSettingList", "", self, "variant_export_setting", self, "variant_export_setting_index", rows = num_rows)
+            layout.template_list("ZUSI_UL_ZusiFileVariantExportSettingList", "", self, "variant_export_setting", self, "variant_export_setting_index", rows = num_rows)
         else:
             box = layout.box()
             box.label(_("Variants can be defined in the Scene settings."))