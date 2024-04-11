35d2aa7ab376a3536600c7f3935a954a2959920c
Johannes <zusitools@web.de>
2024-04-11 09:30:24 +0200

    Version 2.0.25

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@
 bl_info = {
     'name': 'Zusi Scenery Format (.ls3)',
     'author': 'Johannes',
-    'version': (2, 0, 24),
+    'version': (2, 0, 25),
     'blender': (2, 80, 0),
     'location': 'File > Import -> Zusi Scenery (.ls3) / File -> Export > Zusi Scenery (.ls3)',
     'description': 'Import and export files from/to the Zusi Scenery format (.ls3)',
